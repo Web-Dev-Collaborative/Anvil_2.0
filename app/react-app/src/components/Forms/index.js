209f7f538056693d39126e2f43d98836fc42c159
William Vincent <compelledtograce@gmail.com>
2021-03-07 11:01:33 -0500

    removed old version of react

--- app/react-app/src/components/Forms/index.js
+++
@@ -1,6 +0,0 @@
-import NewFolder from "./NewFolder";
-import EditFolder from "./EditFolder";
-import LoginForm from "./Login";
-import SignUpForm from "./Signup";
-
-export { NewFolder, EditFolder, LoginForm, SignUpForm };
--- app/react-app/src/components/auth/index.js
+++
@@ -1,5 +0,0 @@
-import LogoutButton from "./LogoutButton";
-import ProtectedRoute from "./ProtectedRoute";
-import DemoUserButton from "./DemoUserButton";
-
-export { LogoutButton, ProtectedRoute, DemoUserButton };
