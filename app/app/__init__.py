106c35ffba98438d3be5c267b7ebd290f175ecd3
William Vincent <compelledtograce@gmail.com>
2021-03-18 12:48:43 -0400

    troubleshooting flask react app static files

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -65,7 +65,6 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
-    print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico', 'manifest.json', 'demo_logged_in_landing.png')
+    if path in ('favicon.ico', 'manifest.json', 'demo_logged_in_landing.png'):
+        return app.send_static_file(path)
     return app.send_static_file('index.html')
