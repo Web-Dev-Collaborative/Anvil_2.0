c4f3f6e5ee662bdd6057d1255504b557d75b0c80
William Vincent <compelledtograce@gmail.com>
2021-03-18 09:47:28 -0400

    added image meta tag and redirected to landing page on login

--- app/react-app/src/components/auth/DemoUserButton.jsx
+++ app/react-app/src/components/auth/DemoUserButton.jsx
@@ -9,7 +9,7 @@ const DemoUserButton = ({ setLocation }) => {
   const loginDemo = async (e) => {
     dispatch(login({ email: "demo@aa.io", password: "password" }));
     setLocation("/");
-    history.push("/home");
+    history.push("/");
   };
 
   return (
